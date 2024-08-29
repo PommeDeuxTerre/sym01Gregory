1a5143390bdb74091b938dea246cd81c44bfbdae
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-08-29 15:49:57 +0200

    pages

---
+++ src/Controller/PublicController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PublicController extends AbstractController
+{
+    #[Route(
+      path:'/',
+      name: 'homepage'
+    )]
+    public function index(): Response
+    {
+        return $this->render('public/index.html.twig');
+    }
+
+    #[Route(
+      path:'/page/{id}',
+      name: 'page',
+      defaults: ["id"=>1],
+      requirements: ["id"=>"\d+"]
+    )]
+    public function page(int $id): Response
+    {
+        return $this->render('public/page.html.twig', [
+            'nb_page' => $id,
+        ]);
+    }
+
+    #[Route(
+      path:'/page/haha',
+      name: 'error',
+    )]
+    public function error(): Response
+    {
+        return $this->render('public/error.html.twig', [
+        ]);
+    }
+
+    #[Route(
+      path:'/contact',
+      name: 'contact',
+    )]
+    public function contact(): Response
+    {
+        return $this->render('public/contact.html.twig', [
+        ]);
+    }
+}
